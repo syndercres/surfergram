194ef4216e3d971ccd00b0f02647a44ce221b051
syndercombecreswell <syndercombecreswell@gmail.com>
2023-01-10 10:22:39 +0000

    functional

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,8 +1,38 @@
-DROP TABLE IF EXISTS food;
-
-CREATE TABLE  food (
-    id          serial PRIMARY KEY,
-    title       varchar(40) NOT NULL
+CREATE TABLE spots (
+  spot_id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  directions TEXT,
+  rating NUMERIC
 );
 
-INSERT INTO food (title) VALUES ('sourdough'), ('olive oil'), ('pizza'), ('picanha'), ('gorgonzola'), ('basil');
+INSERT INTO spots (name, directions, rating)
+VALUES
+  ('Portland Harbour', 'SW,W,S', 7),
+  ('Poole Harbour', 'SW,E,SE', 8),
+  ('Sandbanks', 'All directions', 9),
+  ('Exmouth', 'N,NW,NE', 6),
+  ('Bournemouth Beach', 'S,SE', 7.5);
+
+CREATE TABLE comments (
+  comment_id SERIAL PRIMARY KEY,
+  spot_id INTEGER REFERENCES spots(spot_id),
+  name TEXT NOT NULL,
+  comment TEXT NOT NULL,
+  rating INTEGER
+);
+
+
+INSERT INTO comments (spot_id, name, comment, rating)
+VALUES
+  (1, 'John', 'This spot is amazing!', 9),
+  (1, 'Alice', 'I had a great time here.', 8),
+  (1, 'Bob', 'I will definitely come back.', 7),
+  (2, 'Alice', 'The directions were a little confusing.', 6),
+  (2, 'Bob', 'I got lost a few times.', 5),
+  (2, 'John', 'I found it eventually though.', 7),
+  (3, 'Bob', 'This spot was breathtaking.', 9),
+  (3, 'Alice', 'I highly recommend it.', 8),
+  (4, 'John', 'I did not like this spot at all.', 2),
+  (4, 'Bob', 'I agree, it was not very enjoyable.', 3);
+
+
--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,13 @@ import { Client } from "pg";
 import { getEnvVarOrFail } from "./support/envVarUtils";
 import { setupDBClientConfig } from "./support/setupDBClientConfig";
 
-dotenv.config(); //Read .env file lines as though they were env vars.
+//--------------------------------------------------------------------------------Read .env file lines as though they were env vars.
+dotenv.config(); 
 
 const dbClientConfig = setupDBClientConfig();
 const client = new Client(dbClientConfig);
 
-//Configure express routes
+//--------------------------------------------------------------------------------Configure express routes
 const app = express();
 
 app.use(express.json()); //add JSON body parser to each following route handler
@@ -31,7 +32,70 @@ app.get("/health-check", async (req, res) => {
     res.status(500).send("An error occurred. Check server logs.");
   }
 });
+//--------------------------------------------------------------------------------Gets all pastes from database
+app.get("/spots", async (req, res) => {
+  const pasteList = await client.query(
+    "SELECT * FROM spots ORDER BY name"
+  );
+  res.status(200).json(pasteList);
+});
+//--------------------------------------------------------------------------------Posts a new paste and adds it to database
+app.post("/spots", async (req, res) => {
+    // to be rigorous, ought to handle non-conforming request bodies
+    // ... but omitting this as a simplification
+    const newSpotName = req.body.name;
+    const newSpotDirections = req.body.directions;
+    const newSpotRating = req.body.rating;
+    const newSpotDescription = req.body.description
+    const text =  "INSERT INTO spots(name, directions, rating, description) VALUES($1, $2, $3, $4) RETURNING *";
+    const values = [newSpotName, newSpotDirections, newSpotRating, newSpotDescription];
+  
+    const postData = await client.query(text, values);
+  
+    res.status(201).json(postData);
+  });
 
+//--------------------------------------------------------------------------------Deletes all data from database
+app.delete("/delete", async (req, res) => {
+  try {
+    await client.query("DELETE FROM comments");
+    await client.query("DELETE FROM spots");
+
+    res.status(200).send("Deleted all!");
+  } catch (error) {
+    //Recover from error rather than letting system halt
+    console.error(error);
+    res.status(500).send("An error occurred. Check server logs.");
+  }
+});
+//--------------------------------------------------------------------------------Adds new comment to comment table
+app.post("/comments", async (req, res) => {
+  // to be rigorous, ought to handle non-conforming request bodies
+  // ... but omitting this as a simplification
+  const spotId = req.body.spot_id;
+  const newCommentName = req.body.name;
+  const newComment = req.body.comment;
+  const newCommentRating = req.body.rating
+  const text =  "INSERT INTO comments(spot_id,name, comment,rating) VALUES($1, $2, $3, $4) RETURNING *";
+  const values = [spotId, newCommentName, newComment, newCommentRating];
+
+  const postData = await client.query(text, values);
+
+  res.status(201).json(postData);
+});
+//--------------------------------------------------------------------------------gets comments from comment table
+app.get("/comments", async (req, res) => {
+  const commentList = await client.query(
+    "SELECT * FROM comments ORDER BY comment_id DESC"
+  ); // await client.query('select "id", "name", "text" from paste_bin');
+  res.status(200).json(commentList);
+  // app.get("/pastes", (req, res) => {
+  //   const allSignatures = getAllDbItems();
+  //   res.status(200).json(allSignatures);
+  // });
+});
+
+//--------------------------------------------------------------------------------Connecting to database
 connectToDBAndStartListening();
 
 async function connectToDBAndStartListening() {
